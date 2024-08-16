09b11d3d578fcc382faeb1e31b2f2c39662e4bcc
samlovat <a1813474@adelaide.edu.au>
2024-08-16 15:50:25 +0930

    almost

--- main.cpp
+++ main.cpp
@@ -1,20 +1,155 @@
 #include <iostream>
 using namespace std;
 #include <string.h>
-#include <deque>
 #include <cmath>
+
+string mult(string I1, string I2, int B){
+    int digit;
+    int carry = 0;
+    string answer = "Multiplication didnt work";
+    if(I1.length() >= I2.length()){
+        answer = "";
+        for(int i = 0; i < I1.length(); i++){
+            digit = (I1[I1.length()-1-i] - '0')*(I2[0] - '0') + carry;
+            if(digit >= B){
+                carry = floor(digit/B);
+                digit = digit%B;
+            }else{
+                carry = 0;
+            }
+            //Prepend next digit to front of answer
+            answer = to_string(digit) + answer;
+        }
+        answer = to_string(carry) + answer;
+    }else{
+        answer = "";
+        for(int i = 0; i < I2.length(); i++){
+            digit = (I2[I2.length()-1-i] - '0')*(I1[0] - '0') + carry;
+            if(digit >= B){
+                carry = floor(digit/B);
+                digit = digit%B;
+            }else{
+                carry = 0;
+            }
+            //Prepend next digit to front of answer
+            answer = to_string(digit) + answer;
+        }
+        answer = to_string(carry) + answer;
+    }
+    return answer;
+}
+
+string add(string I1, string I2, int B){
+    string answer;
+    int length1 = I1.length();
+    int length2 = I2.length();
+    int digit;
+    int Carry = 0;
+    if(I1.length() >= I2.length()){
+        for(int i = 0; i < I1.length(); i++){
+            if(i >= I2.length()){
+                digit = Carry + I1[length1-1-i] - '0';
+            }else{
+                digit = I1[length1-1-i] + I2[length2-1-i] + Carry - '0' - '0';
+            }
+            if(digit < B){
+                Carry = 0;
+            }else{
+                Carry = 1;
+                digit = digit - B;
+            }
+            answer = to_string(digit) + answer;
+        }
+    }else{
+        for(int i = 0; i < I2.length(); i++){
+            if(i >= I1.length()){
+                digit = Carry + I2[length2-1-i] - '0';
+            }else{
+                digit = I2[length2-1-i] + I1[length1-1-i] + Carry - '0' - '0';
+            }
+            if(digit < B){
+                Carry = 0;
+            }else{
+                Carry = 1;
+                digit = digit - B;
+            }
+            answer = to_string(digit) + answer;
+        }
+    }
+
+    //Catch any remaining carry
+    if(Carry == 1){
+        answer = '1' + answer;
+    }
+    return answer;
+}
+
+string karatsuba(string I1, string I2, int B){
+    string answer;
+    //Find Length of numbers
+    int n = max(I1.length(), I2.length());
+
+    //Base Case for n = 1
+    if(I1.length() == 1 || I2.length() == 1){
+        answer = mult(I1, I2, B);
+        return answer;
+    }
+    
+    //Append leading zeros if needed 
+    int numOfAppend;
+    if(I1.length() != n){
+        numOfAppend = n - I1.length();
+        for(int i = 0; i < numOfAppend; i++){
+            I1 = '0' + I1; 
+        }
+    }else{
+        numOfAppend = n - I2.length();
+        for(int i = 0; i < numOfAppend; i++){
+            I2 = '0' + I2; 
+        }
+    }
+
+    //split numbers into parts 
+    string x1, x0, y1, y0;
+    int half = ceil(n/2);
+
+    for(int i = 0; i < n; i++){
+        if(i < half){
+            x1 += I1[i];
+            y1 += I2[i];
+        }else{
+            x0 += I1[i];
+            y0 += I2[i];
+        }
+    }
+
+    cout << "\n\nx0: " << x0 << "\nx1: " << x1 << "\ny0: " << y0 << "\ny1: " << y1 << endl;
+    
+    //Make z2, z1 and z0
+    string z2, z1, z0;
+
+    //Recursive call to z's
+    z2 = karatsuba(x1, y1, B);
+    z1 = karatsuba(x1, y0, B);
+    z0 = karatsuba(x0, y0, B);
+
+    //Append zeros for return
+    for(int i = 0; i < n; i++){
+        z2 += '0';
+    }
+    for(int i = 0; i < half; i++){
+        z1 += '0';
+    }
+
+    answer = add(add(z2, z1, B), z0, B);
+    //return
+    return answer;
+}
 
 int main(){
     string I1;                      //Initialise Values
     string I2;
     int B;
-    std::deque<int> ans;
-    // struct Add{
-    //     std::deque<int> larger;              //Initialise struct of deques to contain all numbers
-    //     std::deque<int> smaller;
-    //     std::deque<int> answer;
-    // };
-    // Add myStruct;
     
     //Scanning for values
     cin >> I1;
@@ -29,13 +164,9 @@ int main(){
         return 1;
     }
 
-    //Fill deques with user input so that larger number is stored in "larger" and smaller is stored in "smaller"
-    //Numbers are being stored backwards so that it is easier later to iterate through the 
-    //deques and perform operations (starting from i = 0). Answer will be flipped.
 
     //Only start calculating length of I1 or I2 once leading zeroes are accounted for
-    int length1 = I1.length();
-    int length2 = I2.length();
+    
     /*
     int counter; 
     int leadingZeros = 1;
@@ -93,46 +224,10 @@ int main(){
         length1++;
     }
     */
-    int digit;
-    int Carry = 0;
-    int larger;
-    if(I1.length() >= I2.length()){
-        for(int i = 0; i < I1.length(); i++){
-            if(i >= I2.length()){
-                digit = Carry + I1[length1-1-i] - '0';
-            }else{
-                digit = I1[length1-1-i] + I2[length2-1-i] + Carry - '0' - '0';
-            }
-            if(digit < B){
-                Carry = 0;
-            }else{
-                Carry = 1;
-                digit = digit - B;
-            }
-            ans.push_front(digit);
-        }
-    }else{
-        for(int i = 0; i < I2.length(); i++){
-            if(i >= I1.length()){
-                digit = Carry + I2[length2-1-i] - '0';
-            }else{
-                digit = I2[length2-1-i] + I1[length1-1-i] + Carry - '0' - '0';
-            }
-            if(digit < B){
-                Carry = 0;
-            }else{
-                Carry = 1;
-                digit = digit - B;
-            }
-            ans.push_front(digit);
-        }
-    }
-    if(Carry == 1){
-        ans.push_front(1);
-    }
-    for(int i = 0; i < ans.size(); i++){
-        cout << ans[i];
-    };
+    string ansadd = add(I1, I2, B);
+    string ansmult = karatsuba(I1, I2, B);
+    cout << "\n\nAnsadd: " << ansadd << endl;
+    cout << "\n\nAnsmult: " << ansmult << endl;
     
     return 0;
 }
