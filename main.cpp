dc242724aafeffa8298f9ce84bf971213df80336
samlovat <a1813474@adelaide.edu.au>
2024-08-18 16:42:13 +0930

    DONE

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ string mult(string I1, string I2, int B){
     string answer = "Multiplication didnt work";
     if(I1.length() >= I2.length()){
         answer = "";
-        for(int i = 0; i < I1.length(); i++){
+        for(size_t i = 0; i < I1.length(); i++){
             digit = (I1[I1.length()-1-i] - '0')*(I2[0] - '0') + carry;
             if(digit >= B){
                 carry = floor(digit/B);
@@ -23,7 +23,7 @@ string mult(string I1, string I2, int B){
         answer = to_string(carry) + answer;
     }else{
         answer = "";
-        for(int i = 0; i < I2.length(); i++){
+        for(size_t i = 0; i < I2.length(); i++){
             digit = (I2[I2.length()-1-i] - '0')*(I1[0] - '0') + carry;
             if(digit >= B){
                 carry = floor(digit/B);
@@ -46,7 +46,7 @@ string add(string I1, string I2, int B){
     int digit;
     int Carry = 0;
     if(I1.length() >= I2.length()){
-        for(int i = 0; i < I1.length(); i++){
+        for(size_t i = 0; i < I1.length(); i++){
             if(i >= I2.length()){
                 digit = Carry + I1[length1-1-i] - '0';
             }else{
@@ -61,7 +61,7 @@ string add(string I1, string I2, int B){
             answer = to_string(digit) + answer;
         }
     }else{
-        for(int i = 0; i < I2.length(); i++){
+        for(size_t i = 0; i < I2.length(); i++){
             if(i >= I1.length()){
                 digit = Carry + I2[length2-1-i] - '0';
             }else{
@@ -108,7 +108,7 @@ string subt(string I1, string I2, int B){
             }
         }
     }
-    for(int i = 0; i < I1.length(); i++){
+    for(size_t i = 0; i < I1.length(); i++){
         if((I1[i] - '0') < (I2[i] - '0')){
             larger = I2;
             smaller = I1;
@@ -127,10 +127,9 @@ string subt(string I1, string I2, int B){
         return answer;
     }
 
-
     //Subtraction
     int steal = 0;
-    for(int i = 0; i < smaller.length(); i++){
+    for(size_t i = 0; i < smaller.length(); i++){
         if(steal != 1){
             digit1 = larger[larger.length()-1-i] - '0'; 
             digit2 = smaller[smaller.length()-1-i] - '0';
@@ -152,11 +151,8 @@ string subt(string I1, string I2, int B){
 
 string karatsuba(string I1, string I2, int B){
     string answer;
-    static int counter = 0;
-    counter++;
     //Find Length of numbers
     float n = max(I1.length(), I2.length());
-    // std::cout << "Karatsuba Call Num: " << counter << "\nFor the I1 and I2 of " << I1 << " and " << I2 << ". The n value is " << n << endl;
 
     //Base Case for n = 1
     if(I1.length() == 1 || I2.length() == 1){
@@ -190,8 +186,6 @@ string karatsuba(string I1, string I2, int B){
             y0 += I2[i];
         }
     }
-
-    // cout << "x0: " << x0 << " x1: " << x1 << " y0: " << y0 << " y1: " << y1 << endl;
     
     //Make z2, z1 and z0
     string P0, P1, P2;
@@ -200,10 +194,9 @@ string karatsuba(string I1, string I2, int B){
     P0 = karatsuba(x0, y0, B);
     P1 = karatsuba(x1, y1, B);
     P2 = karatsuba((add(x0, x1, B)), add(y0, y1, B), B);
-    // std::cout << "Karatsuba Num: " << counter << ". \nP0: " << P0 << " P1: " << P1 << "P2: " << P2 << endl;
+
     //Subtract to find P2 - P1 - P0
     string sub = subt(subt(P2, P1, B), P0, B);
-    // std::cout << "Sub Val: " << sub << endl;
 
     //Append zeros for return
     for(int i = 0; i < (half*2); i++){
@@ -214,8 +207,6 @@ string karatsuba(string I1, string I2, int B){
     }
 
     answer = add(add(P1, sub, B), P0, B);
-    // std::cout << "Final Calculation: P1*10^n: " << P1 << " ()*10^n/2: " << sub << " P0: " << P0 << endl;
-    //return
     return answer;
 }
 
@@ -228,7 +219,6 @@ int main(){
     cin >> I1;
     cin >> I2;
     cin >> B;
-    // cout << "I1: " << I1 << "\n\nI2: " << I2 << "\n\nB: " << B << endl;
 
     /*Error Checking*/
 
@@ -237,74 +227,12 @@ int main(){
         return 1;
     }
 
-
-    //Only start calculating length of I1 or I2 once leading zeroes are accounted for
-    
-    /*
-    int counter; 
-    int leadingZeros = 1;
-    while(leadingZeros == 1){
-        if(I1[counter] == '\0'){
-            break;
-        }
-        if(I1[counter] == '0'){
-            counter++;
-        }else{
-            leadingZeros = 0;
-        }
-    }
-    length1 = I1.length() - counter;
-
-    counter = 0;
-    leadingZeros = 1;
-    while(leadingZeros == 1){
-        if(I2[counter] == '\0'){
-            break;
-        }
-        if(I2[counter] == '0'){
-            counter++;
-        }else{
-            leadingZeros = 0;
-        }
-    }
-    length2 = I2.length() - counter;
-
-    if(length1 > length2){
-
-    }
-    digit = I2[counter] - '0';
-    myStruct.larger.push_back(digit);
-    counter++;
-    length1++;
-
-    leadingZeros = 1;
-    counter = 0;
-    while(I2[counter] != '\0'){
-        while(leadingZeros == 1){
-            if(I2[counter] == '\0'){
-                break;
-            }
-            digit = I1[counter] - '0';
-            myStruct.larger.push_back(digit);
-            counter++;
-            if(digit != 0){
-                leadingZeros = 0;
-            }
-        }
-        digit = I1[counter] - '0';
-        myStruct.larger.push_back(digit);
-        counter++;
-        length1++;
-    }
-    */
-
-
     string ansadd = add(I1, I2, B);
     string ansmult = karatsuba(I1, I2, B);
 
     //Cleans ansmult of leading zeros
     int leadingZero;
-    for(int i = 0; i < ansmult.length(); i++){
+    for(size_t i = 0; i < ansmult.length(); i++){
         if(ansmult[i] == '0'){
             leadingZero = 1;
         }else{
@@ -316,8 +244,5 @@ int main(){
         }
     }
     std::cout << ansadd << " " << ansmult << " 0" << endl;   
-
-    // string test = mult("8", "71827387", 9); 
-    // std::cout << "\n\nTEST RESULTS: " << test << endl;
     return 0;
 }
