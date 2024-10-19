a809e0bd3a5791a6a28bc7e409e626aa2ed65a5a
samlovat <a1813474@adelaide.edu.au>
2024-10-19 19:14:54 +1030

    we begin

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <string.h>
+
+int main(){
+    //Read input
+    string input;
+    cin << input;
+
+    //Collect Strongly Linked Components
+
+    return 0;
+}
